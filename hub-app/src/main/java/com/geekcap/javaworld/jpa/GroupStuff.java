fc6e673b87d1f7b7172cdb169c01c84b85a375d0
avi bernstein <abernst4@mail.yu.edu>
2022-07-07 15:53:13 -0400

    me

---
+++ hub-app/src/main/java/com/geekcap/javaworld/jpa/GroupStuff.java
@@ -0,0 +1,33 @@
+package main.java.com.geekcap.javaworld.jpa;
+
+import java.net.InetAddress;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.*;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+public class GroupStuff extends PanacheEntity {// why isn't there an error here
+    @Id
+    public long groupId;
+
+    public InetAddress ia;
+
+    public GroupStuff(long GroupId, InetAddress ia) {
+        this.groupId = GroupId;
+        this.ia = ia;
+    }
+
+    public Map<Long, InetAddress> toMap() {
+        Map<Long, InetAddress> IdMap = new HashMap<>();
+        List<galleryInfo> groupList = GroupStuff.listAll();
+
+        for (GroupStuff gs : groupList) {
+            IdMap.put(gs.groupId, gs.ia);
+        }
+
+        return IdMap;
+    }
+}
